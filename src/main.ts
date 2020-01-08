bc23ca368ecf99b7c4c3aff980f5f44bbcea4ae9
Cleivson Siqueira De Arruda <cleivson.tb@gmail.com>
2020-01-07 22:12:34 -0300

    Setup Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  configureSwagger(app);
+
+  app.useLogger(console);
+
   await app.listen(3000);
 }
+
+function configureSwagger(app) {
+  const options = new DocumentBuilder()
+    .setTitle('Jogging Tracker')
+    .setDescription('Jogging Tracker is an API for registering jogging activities and keeping track of exercises.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
