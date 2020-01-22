924e5721f180b7d0c280c467de5ddc6f5226822a
Cleivson Siqueira De Arruda <cleivson.tb@gmail.com>
2020-01-22 01:43:04 -0300

    Fix route order problem with weekly report

--- src/jogging/user-jogging.controller.ts
+++ src/jogging/user-jogging.controller.ts
@@ -1,7 +1,7 @@
 import { ClassSerializerInterceptor, Controller, Delete, ForbiddenException, Get, HttpStatus, Param, ParseIntPipe, Patch, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Crud, CrudRequest, CrudRequestInterceptor, GetManyDefaultResponse, Override, ParsedBody, ParsedRequest } from '@nestjsx/crud';
+import { Crud, CrudRequest, CrudRequestInterceptor, Override, ParsedBody, ParsedRequest } from '@nestjsx/crud';
 import { LoggedUser } from 'auth';
 import { AdvancedQuery, ParsedQuery } from 'query';
 import { LoggedUserDto, Roles, RolesGuard, UserRoles } from 'users';
@@ -38,7 +38,19 @@ const USER_ID = 'userId';
 export class UserJoggingController {
   constructor(private readonly service: JoggingService) { }
 
-  @Get()
+  @Get('/weekly-report')
+  @ApiOperation({ summary: 'Generate jogging weekly report for a specific user' })
+  @ApiResponse({ status: HttpStatus.OK, type: WeeklyReportDto, isArray: true })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Logged user cannot generate report of jogging entries for specified user.' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found.' })
+  async generateWeeklyReport(@Param(USER_ID, ParseIntPipe) pathUserId: number,
+                             @LoggedUser() user: LoggedUserDto): Promise<WeeklyReportDto[]> {
+
+    this.validateJoggingEntryOwner(user, pathUserId);
+
+    return this.service.generateWeeklyReport(pathUserId);
+  }
+
   @Override()
   @ApiResponse({ status: HttpStatus.OK, type: JoggingEntry })
   @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Logged user cannot retrieve jogging entries of specified user.' })
@@ -136,19 +148,6 @@ export class UserJoggingController {
     this.validateJoggingEntryOwner(user, pathUserId);
 
     return this.service.getJoggingEntries(req, query, pathUserId);
-  }
-
-  @Get('/weekly-report')
-  @ApiOperation({ summary: 'Generate jogging weekly report for a specific user' })
-  @ApiResponse({ status: HttpStatus.OK, type: WeeklyReportDto, isArray: true })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Logged user cannot generate report of jogging entries for specified user.' })
-  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found.' })
-  async generateWeeklyReport(@Param(USER_ID, ParseIntPipe) pathUserId: number,
-                             @LoggedUser() user: LoggedUserDto): Promise<GetManyDefaultResponse<WeeklyReportDto> | WeeklyReportDto[]> {
-
-    this.validateJoggingEntryOwner(user, pathUserId);
-
-    return this.service.generateWeeklyReport(pathUserId);
   }
 
   private validateJoggingEntryOwner(user: LoggedUserDto, pathUserId: number) {
